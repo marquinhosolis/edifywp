e5d3417a726f87a8ba0368c653bee64530622793
Marco Solis <marquinhosolis@gmail.com>
2022-03-02 18:34:55 -0300

    Adicionando footer

--- themes/edifyTheme/src/footer.php
+++ themes/edifyTheme/src/footer.php
@@ -1,5 +1,54 @@
 <footer>
-
+    <div class="footerRegular">
+        <div class="container">
+            <div class="footerLogo">
+                <img src="<?php echo get_stylesheet_directory_uri()?>/images/logo.svg" alt="Reading.com logo" />
+                <p>
+                    Reading.com é um produto fictício para ajudar no
+                    processo seletivo para o time de Produto &
+                    Desenvolvimento da Hivecloud.
+                </p>
+            </div>
+            <div class="footerLinks">
+                <ul>
+                    <strong>Reading.com</strong>
+                    <li>Meus livros</li>
+                    <li>Comunidade</li>
+                    <li>Novidades</li>
+                    <li>Aplicativos</li>
+                </ul>
+            </div>
+            <div class="footerLinks">
+                <ul>
+                    <strong>Sobre Nós</strong>
+                    <li>Blog</li>
+                    <li>Nossa missão</li>
+                    <li>Contato</li>
+                    <li>Carreiras</li>
+                </ul>
+            </div>
+            <div class="footerForm">
+                <strong>Inscreva-se</strong>
+                <input type="text" placeholder="Seu e-mail" />
+                <Button styleName="btn-default2">Inscreva-se</Button>
+            </div>
+        </div>
+    </div>
+    <div class="footerLegal">
+        <div class="container">
+            <p>Copyright © 2020 Reading All rights reserved</p>
+            <ul>
+                <li>Política de privacidade</li>
+                <li>Termos de uso</li>
+                <li>Segurança</li>
+                <li>Legal</li>
+                <li>Site map</li>
+            </ul>
+            <p>
+                PT <IoChevronUpOutline />
+            </p>
+        </div>
+    </div>
 </footer>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/lib/jquery.min.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
